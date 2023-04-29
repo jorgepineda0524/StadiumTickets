5bed3ce862e1cfd83cbb635e251334f569c92ad6
Jorge Pineda <jpineda@pasteur.com.co>
2023-04-29 08:41:35 -0500

    confirm ticket and ticket not valid

--- StadiumTickets.WEB/Program.cs
+++ StadiumTickets.WEB/Program.cs
@@ -1,6 +1,8 @@
+using Blazored.Modal;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using StadiumTickets.WEB;
 using StadiumTickets.WEB.Repositories;
 
@@ -12,5 +14,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7065/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
-
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredModal();
 await builder.Build().RunAsync();
